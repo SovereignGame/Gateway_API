6568737464dd300fb0cb4ed4c09c89fbe9890f44
Martin Wangen <wangen93@hotmail.com>
2020-04-14 12:15:16 +0200

    gateway with loadbalancing and Eureka nameing server

--- src/test/java/com/sovreign/gateway/GatewayApplicationTests.java
+++ src/test/java/com/sovreign/gateway/GatewayApplicationTests.java
@@ -1,13 +1,23 @@
 package com.sovreign.gateway;
 
-import org.junit.jupiter.api.Test;
+import org.junit.Test;
+import org.junit.runner.RunWith;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.test.autoconfigure.web.reactive.AutoConfigureWebTestClient;
 import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.cloud.contract.wiremock.AutoConfigureWireMock;
+import org.springframework.test.context.junit4.SpringRunner;
+import org.springframework.test.web.reactive.server.WebTestClient;
+
+import static com.github.tomakehurst.wiremock.client.WireMock.*;
+import static org.assertj.core.api.Assertions.*;
+
 
 @SpringBootTest
-class GatewayApplicationTests {
+public class GatewayApplicationTests {
 
 	@Test
-	void contextLoads() {
-	}
+	public void contextLoads() throws Exception {
 
+	}
 }
